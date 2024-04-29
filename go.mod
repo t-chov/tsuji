848e80fb0ae6f843245073ec238899a3e27978c2
Kohei Tsuyuki <kotsuyuki@gmail.com>
2024-04-29 21:03:56 +0900

    use urfave/cli

--- go.mod
+++ go.mod
@@ -3,18 +3,29 @@ module github.com/t-chov/tsuji
 go 1.22.2
 
 require (
+	github.com/aws/aws-sdk-go-v2/config v1.27.11
+	github.com/aws/aws-sdk-go-v2/service/bedrock v1.8.0
+	github.com/fatih/color v1.16.0
+	github.com/urfave/cli/v2 v2.27.2
+)
+
+require (
 	github.com/aws/aws-sdk-go-v2 v1.26.1 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.27.11 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.11 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.1 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.5 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.5 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/bedrock v1.8.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.7 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.20.5 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.23.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.28.6 // indirect
 	github.com/aws/smithy-go v1.20.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
+	golang.org/x/sys v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,30 +3,74 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/bedrock"
+	"github.com/fatih/color"
+	"github.com/urfave/cli/v2"
 )
 
-const OREGON = "us-west-2"
+const (
+	OREGON     = "us-west-2"
+	errorColor = color.FgRed
+)
 
 func main() {
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(OREGON))
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
+	os.Exit(run(initApp()))
+}
 
-	br := bedrock.NewFromConfig(cfg)
-	result, err := br.ListFoundationModels(context.TODO(), &bedrock.ListFoundationModelsInput{})
+func run(app *cli.App) int {
+	return msg(app.Run(os.Args))
+}
+
+func msg(err error) int {
 	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
+		red := color.New(errorColor).FprintfFunc()
+		red(os.Stderr, "%s: %v\n", os.Args[0], err)
+		return 1
 	}
-	if len(result.ModelSummaries) == 0 {
-		fmt.Println("There are no foundation models.")
+	return 0
+}
+
+func initApp() *cli.App {
+	app := cli.NewApp()
+	app.Name = "tsuji"
+	app.Usage = "an English learning tool utilizing Generative AI."
+	app.Version = "0.0.1"
+	app.Commands = []*cli.Command{
+		{
+			Name:  "list",
+			Usage: "list available models",
+			Action: func(ctx *cli.Context) error {
+				cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(OREGON))
+				if err != nil {
+					return err
+				}
+
+				br := bedrock.NewFromConfig(cfg)
+				result, err := br.ListFoundationModels(context.TODO(), &bedrock.ListFoundationModelsInput{})
+				if err != nil {
+					return err
+				}
+				if len(result.ModelSummaries) == 0 {
+					fmt.Println("There are no foundation models.")
+				}
+				for _, modelSummary := range result.ModelSummaries {
+					fmt.Println(*modelSummary.ModelId)
+				}
+				return nil
+			},
+		},
 	}
-	for _, modelSummary := range result.ModelSummaries {
-		fmt.Println(*modelSummary.ModelId)
+	app.Action = appRun
+	return app
+}
+
+func appRun(c *cli.Context) error {
+	args := c.Args()
+	if !args.Present() {
+		cli.ShowAppHelp(c)
 	}
+	return nil
 }
